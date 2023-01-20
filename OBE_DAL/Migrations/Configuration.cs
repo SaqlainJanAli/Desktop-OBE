b19298cdf247d0a63d7bf7624c98d10a1cb1a5f0
Saqlain Mushtaq <88180325+SaqlainJanAli@users.noreply.github.com>
2023-01-20 12:31:45 +0500

    Add files via upload

---
+++ OBE_DAL/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace OBE_DAL.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OBE_DAL.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(OBE_DAL.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
